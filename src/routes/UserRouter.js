1401757a0566d76b465f1a6942563620a66eb68f
gstvmorais <guga.rodrigues11.0@hotmail.com>
2022-02-08 11:07:25 -0300

    Shortner Controller, Router and Model

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,4 +1,5 @@
-import Router from express;
+import { Router } from "express";
+import UserController from "../controller/UserController.js";
 
 const router = Router();
 
@@ -6,10 +7,10 @@ router.get("/user", UserController.index);
 
 router.get("/user/:id", UserController.getOne);
 
-router.post("/user",  UserController.store);
+router.post("/user", UserController.store);
 
 router.put("/user/:id", UserController.update);
 
 router.delete("/user/:id", UserController.remove);
 
-export default Router;
+export default router;
